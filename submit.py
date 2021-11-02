6f69d7e98df59311cae0b95f5ef71cef60cdccd9
Md Ashraful Islam <ashrafulcse.sust@gmail.com>
2021-11-02 01:35:04 -0500

    Refactored

--- submit.py
+++ submit.py
@@ -1,11 +1,13 @@
+from sys import argv
+
 from action import HostAction
 from config import KombuConfig
 from producer import EventProducer
 
 data = {
-    'k': 3,
-    'e': 0,
-    'path': 'data/500_Person_Gender_Height_Weight_Index.csv'
+    'k': int(argv[1]),
+    'e': int(argv[2]),
+    'path': argv[3]
 }
 
 producer = EventProducer()
